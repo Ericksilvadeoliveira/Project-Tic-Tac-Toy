9a22a9e7d587c5d28e8bc800f6d4ff85df52529c
Erick Silva <106249621+Ericksilvadeoliveira@users.noreply.github.com>
2022-12-08 08:31:17 -0300

    estruturando o html

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Project-Tic-Tac-Toy
  Javascript Course - The Odin Project
+
+
+We’re making a Tic Tac Toe game you can play in your browser!
+
---
+++ ticTacToe.js
@@ -0,0 +1,8 @@
+const casas = Array.from(document.querySelectorAll('.casas'));
+
+const playerDisplay = document.querySelector('.display-player');
+
+const resultado = document.querySelector('#resultado');
+
+
+
