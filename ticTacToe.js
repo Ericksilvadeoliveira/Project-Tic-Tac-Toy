5ed6d7232b40f3eadaa8e29885a5acac2c77a8f6
Erick Silva <106249621+Ericksilvadeoliveira@users.noreply.github.com>
2022-12-09 11:43:35 -0300

    nao sei carai addeventlistener

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,8 +1,13 @@
-const casas = Array.from(document.querySelectorAll('.casas'));
+const casas = [...document.querySelectorAll('.casas')];
 
 const playerDisplay = document.querySelector('.display-player');
 
 const resultado = document.querySelector('#resultado');
 
-
+casas.forEach(item => {
+    item.addEventListener('click', () => {
+        item.innerHTML = 'x';
+        console.log(casas)
+    })
+});
 
