5e4ddbc43fb22eca74f9f2a17fd64a1b40cdaec1
Erick Silva <106249621+Ericksilvadeoliveira@users.noreply.github.com>
2022-12-10 13:50:02 -0300

    como alterar a string?

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,13 +1,22 @@
-const casas = [...document.querySelectorAll('.casas')];
+'use strict'
+
+const casas = document.querySelectorAll('.casas');
+
+const todasCasas = Array.from(casas);
 
 const playerDisplay = document.querySelector('.display-player');
 
 const resultado = document.querySelector('#resultado');
 
-casas.forEach(item => {
-    item.addEventListener('click', () => {
-        item.innerHTML = 'x';
-        console.log(casas)
-    })
-});
+todasCasas.push(' ', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x');
+console.log(todasCasas);
 
+function clickString (index) {
+    index = todasCasas.indexOf(' ');
+    if (index !== -1) {
+        todasCasas[index] = 'ggiu'
+    }
+}
+
+todasCasas.forEach(clickString);
+console.log(todasCasas);
